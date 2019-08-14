a3c57a11030c0f454513958a8e57df2eb8b6c402
cinker <cdoors@gmail.com>
2019-08-14 12:12:48 +0800

    change

--- tip_calculation.py
+++ tip_calculation.py
@@ -10,17 +10,29 @@ This is a tip calculation
 Enter the tip as a percentage. For example, a 15% tip would be entered as 15, 
 not 0.15. Your program should handle the division.
 Round fractions of a cent up to the next cent.
+
+Ensure that the user can enter only numbers for the bill amount and the tip 
+rate. If the user enters non-numeric values, display an appropriate message 
+and exit the program.
 """
 
+import numbers
+
 def tip_calc(bill_amount, tip_perct):
-    tip_amount = bill_amount*tip_perct
-    total_amount = bill_amount+tip_amount
-    print("The tip is ${0}".format(tip_amount))
-    print("The total is ${0}".format(total_amount))
-    return (tip_amount, total_amount)
+    if isinstance(bill_amount, numbers.Number) and \
+        isinstance(tip_perct, numbers.Number):
+            bill_amount = float(bill_amount)
+            tip_perct = float(tip_calc)/100
+            tip_amount = bill_amount*tip_perct
+            total_amount = bill_amount+tip_amount
+            print("The tip is ${0}".format(tip_amount))
+            print("The total is ${0}".format(total_amount))
+            return (tip_amount, total_amount)
+    else:
+        print("Please enter a valid number for the bill amount.")
 
 if __name__ == "__main__":
-    bill_amount = float(input("What is the bill? "))
-    tip_perct = int(input("Please input tip percentage: "))/100
+    bill_amount = input("What is the bill? ")
+    tip_perct = input("Please input tip percentage: ")
     tip_calc(bill_amount, tip_perct)
 
